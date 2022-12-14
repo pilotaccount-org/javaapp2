620f2b4b1c7c62f49da84c9bdb64c7df176d59e9
abc <abc@example.com>
2022-12-14 14:43:13 +0530

    first commit

---
+++ src/test/java/com/example/javaapp2/Javaapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
